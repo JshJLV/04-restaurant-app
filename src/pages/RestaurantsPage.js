c40b3f894a3c336850ca9511bc074a4f8c0fec30
Joseph Lopez <joseph.lpz1999@gmail.com>
2023-02-25 19:52:21 -0600

    conexion a base de datos

--- src/pages/RestaurantsPage.js
+++ src/pages/RestaurantsPage.js
@@ -1,7 +1,14 @@
+import FormReservation from "./restaurants/FormReservation";
+
 function RestaurantsPage() {
   return (
-    <div>Restaurants</div>
-  )
+    <main className="container mx-auto flex flex-col xl:flex-row px-10 py-6">
+      <FormReservation />
+      <div className="mb-10 xl:mb-0 xl:flex-1 xl:pr-4">
+        "Seccion de mapa"
+      </div>
+    </main>
+  );
 }
 
-export default RestaurantsPage
+export default RestaurantsPage;
--- src/pages/homepage/FormContact.js
+++ src/pages/homepage/FormContact.js
@@ -1,6 +1,5 @@
-import Inputs from "../../components/Inputs";
-import Button from "../../components/Button";
-import imgRestaurante from "../../assets/img/restaurant.jpg"
+// import Form from "../../components/Form";
+import imgRestaurante from "../../assets/img/restaurant.jpg";
 
 function FormContact() {
   return (
@@ -12,22 +11,36 @@ function FormContact() {
           preguntando lo que necesites
         </p>
         <div className="xl:mt-10">
-          <img src={imgRestaurante} alt="Fotografia de restaurante"/>
+          <img src={imgRestaurante} alt="Fotografia de restaurante" />
         </div>
       </div>
       <form className="xl:flex-1 xl:pl-4 xl:flex xl:flex-col xl:justify-center">
         <label className="text-xl mt-3 block">Nombre: </label>
-        <Inputs placeholder={"Nombre"} />
+        <input
+          className="bg-gray-100 rounded-md mt-2 p-2 w-full text-lg"
+          placeholder="Nombre"
+        />
         <label className="text-xl block mt-3">Teléfono: </label>
-        <Inputs placeholder={"Teléfono"} />
+        <input
+          className="bg-gray-100 rounded-md mt-2 p-2 w-full text-lg"
+          placeholder="Teléfono"
+        />
         <label className="text-xl block mt-3">Email: </label>
-        <Inputs placeholder={"Email"} />
+        <input
+          className="bg-gray-100 rounded-md mt-2 p-2 w-full text-lg"
+          placeholder="Email"
+        />
         <label className="text-xl block mt-3">Mensaje: </label>
         <textarea
           className="bg-gray-100 rounded-md mt-2 p-2 w-full"
           placeholder="Escribe tu mensaje"
         ></textarea>
-        <Button />
+        <button
+          className="bg-green-600 hover:bg-green-400 text-white 
+        text-xl w-full mt-3 px-2 py-2 rounded-md"
+        >
+          Enviar
+        </button>
       </form>
     </div>
   );
